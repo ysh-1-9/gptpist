7a16067bd336d9c10b5c041979beba6432c6ee41
evilyash <evilyash02@gmail.com>
2022-12-11 01:18:37 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,117 @@
+import pandas as pd
+import re
+import json
+
+
+class Prompt:
+    def __init__(self, window=5):
+        self.summary = ""
+        self.specifics = ""
+        self.window = window
+        self.clientMessages = ["" for _ in range(self.window)]
+        self.therapistMessages = ["" for _ in range(self.window)]  # last therapist message is empty
+
+    def to_string(self):
+        s = ""
+        s += "Summary: " + self.summary + "\n\n"
+        s += "Specific Information: " + self.specifics + "\n\n###\n\n"
+        for i in range(self.window):
+            s += "Client: " + self.clientMessages[i] + '\n'
+            s += "Therapist: " + self.therapistMessages[i] + '\n'
+
+        return s[:-1]
+
+
+# final dataset will be list of jsons, each json with one prompt, one completion
+# isko csv mein daalke cli data-prep tool ko de denge
+
+
+def get_summary(s):
+    # takes in the last
+    return f"<summary tbi {len(s)}>"
+    pass
+
+
+def get_specifics(s):
+    return f"<specifics tbi {len(s)}>"
+    pass
+
+
+'''
+{p:"
+    summary:
+    specifics: 
+    client1: good morning
+    t1: 
+    "
+ ,c: "Won't you have a chair? Now..."
+ }
+ 
+ {
+ p:"
+    summary: client has introduced themselves
+    client1: good morning
+    t1: Won't you have a chair? Now...
+    c2: Well, right now I'm nervous...
+    t2: 
+
+json has 2 fields - prompt and completion
+
+1. prompt - 
+    a. Summary - 
+
+'''
+
+
+def ops(line):
+    line = line[line.find(' ') + 1:]
+    # strip all the shit within brackets
+    line = re.sub("[\(\[].*?[\)\]]", "", line)
+    line = line.replace('\n','')
+    return line
+
+
+def convert_to_csv(file, window=5):
+    f = open(file)
+
+    therapist = []
+    client = []
+
+    w = open(file.split('.')[0] + '_proc.jsonl', 'w')
+
+    for line in f:
+        if line[0] == 'T':
+            therapist.append(ops(line))
+        elif line[0] == 'C':
+            client.append(ops(line))
+        else:
+            print('EOF\n')
+
+    therapist = therapist[1:]
+
+    l = min(len(therapist), len(client))
+
+    # therapist[i] is what goes into the completion
+    # client[index] i,i-1,i-2 .. i-(window-1) goes into clientMessages
+    # therapist[index] _,i-1,... i-(window-1) goes into therapistMessages
+    # client[index] i,i-1,...0 goes into summary
+    conv = []
+    prompt = Prompt(window)
+    for i in range(l):
+        conv.append("Client: " + client[i])
+
+        prompt.summary = get_summary(conv)
+        prompt.specifics = get_specifics(conv)
+        prompt.clientMessages = prompt.clientMessages[1:] + [client[i]]
+        prompt.therapistMessages = prompt.therapistMessages[1:-1] + [therapist[i - 1] if i > 0 else ""] + [""]
+
+        completion = ' '+therapist[i]+"<end>"
+
+        j = {"prompt": prompt.to_string(), "completion": completion}
+        print(f'[{i}]: Made JSON: {json.dumps(j)}')
+        w.write(json.dumps(j) + '\n')
+
+        conv.append("Therapist: " + therapist[i])
+
+
+convert_to_csv('gloria.txt')
